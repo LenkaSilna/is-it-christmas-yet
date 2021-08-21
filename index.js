2299f284cce9386c177dda194adde7a16cd579e3
Lenka Silná <lenka.baricova@post.cz>
2021-08-21 11:01:05 +0200

    is it chrismas yet

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+//tady jsou vanoce
+const vanoce = dayjs('2021-12-24');
+//tady je dnešek
+const dnes = dayjs();
+
+const odpoved = document.querySelector('#odpoved');
+
+if (dnes.isAfter(vanoce)) {
+  odpoved.textContent = 'ANO';
+} else {
+  odpoved.textContent = 'NE';
+}
